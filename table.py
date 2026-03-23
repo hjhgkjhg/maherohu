9b8eda1e16acba736cc9569ef6cb363b2cd1b036
leo theeng <163860925+hjhgkjhg@users.noreply.github.com>
2026-03-23 10:37:30 +0545

    Add files via upload

---
+++ table.py
@@ -0,0 +1,8 @@
+n = int(input('enter your number:'))
+i = 1
+q = int(input(f'enterthe number upto which you want mult:'))
+while(i<=q):
+    result = n * i
+    print(f"{n} * {i} = {result}")
+    i = i + 1
+    
